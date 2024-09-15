ccc6aae7cfb531cd8f255b1a917dae2db6b34c81
Sem <semademir.info@gmail.com>
2024-09-14 21:11:21 -0400

    browser tabs+navigations

---
+++ cypress/e2e/other/same-origin-policy.js
@@ -0,0 +1,18 @@
+/// <reference types = "cypress" />
+
+describe("Cypress web Security", () => {
+    it("Validate visiting two different domains", () => {
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.visit('https://automationteststore.com/')
+
+    });
+
+    it.only("Validate visiting two different domains via user actions", () => {
+        cy.visit('http://www.webdriveruniversity.com/')
+        cy.get('#automation-test-store').invoke('removeAttr' , 'target').click()
+
+
+
+    });
+
+})
--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -3,7 +3,9 @@
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
 
-        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        // cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html") //modified and open the new tab in the same new browser
+        cy.visit("https://www.webdriveruniversity.com/")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true }) //removed target attr with jquery remove Attr method 
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'contactus')
@@ -19,11 +21,13 @@ describe("Test Contact Us form via WebdriverUni", () => {
     });
 
     it("Should not be able to submit a successful submission via contact us form as all field are required", () => {
-        cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
-        //cy.get('#contact-us').click({force:true})
+        //cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
+
+        cy.visit("https://www.webdriveruniversity.com/")
+        cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true })
         cy.get('[name="first_name"]').type("John")
         cy.get('[name="last_name"]').type("Smith")
-        cy.get('[name="email"]').type("xxx")
+        cy.get('[name="email"]').type("jsmith.test")
         cy.get('textarea.feedback-input').type("Hello Cypress! It's me! ")
         cy.get('[type="submit"]').click({ force: true })
         cy.get('body').should('contain', 'Error: Invalid email address')
