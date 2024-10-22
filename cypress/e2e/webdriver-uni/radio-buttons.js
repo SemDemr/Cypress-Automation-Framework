eece64227e7bad9c5fedc97ed7f8951d930bac92
Sem <semademir.info@gmail.com>
2024-10-21 21:07:50 -0400

    hooks&fixtures

--- cypress/e2e/webdriver-uni/radio-buttons.js
+++ cypress/e2e/webdriver-uni/radio-buttons.js
@@ -2,16 +2,18 @@
 
 
 describe("Verify radio buttons", () => {
-    it("Check specific radio buttons", () => {
+    before(function () {
         cy.visit('http://www.webdriveruniversity.com/')
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true });
+
+    })
+    it("Check specific radio buttons", () => {
+
         cy.get('#radio-buttons').find("[type='radio']").first().check()
         cy.get('#radio-buttons').find("[type='radio']").eq(2).check()
     })
 
     it.only("Validate the states of specific radio buttons", () => {
-        cy.visit('http://www.webdriveruniversity.com/')
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true });
         cy.get('[value="lettuce"]').should('not.be.checked')
         cy.get('[value="cabbage"]').should('not.be.checked')
         cy.get('[value="pumpkin"]').check()
